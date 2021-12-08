e3057aa346e27a5c2d857a31ceaad296e4b6a925
emattia <emattia4495@gmail.com>
2021-12-08 16:43:11 -0600

    add feature dependence plots

--- examples/regression/boston/runs/explain_xgb_boston.py
+++ examples/regression/boston/runs/explain_xgb_boston.py
@@ -21,4 +21,15 @@ assert shap_values.shape == Xtest.shape
 # waterfall_plot = shap.plots.waterfall(shap_values[0])
 
 force_plot_single_data_pt = shap.plots.force(shap_values[0])
-fig = force_plot_single_data_pt.matplotlib(figsize=(12,6), show=False, text_rotation=0)
+fig_single_data_pt = force_plot_single_data_pt.matplotlib(figsize=(12,6), show=False, text_rotation=0)
+
+### below only works in ipython, uses react instead of matplotlib ;(
+# force_plot_all_test_data = shap.plots.force(shap_values)
+# fig_full_test_set = force_plot_all_test_data.matplotlib(figsize=(12,6), show=False, text_rotation=0)
+
+feature_dependence_plot_figs = []
+for feat_name in shap_values.feature_names:
+  fig, ax = plt.subplots(1,1,figsize=(12,12))
+  shap.plots.scatter(shap_values[:, feat_name], color = shap_values, ax = ax, show = False)
+  feature_dependence_plot_figs.append(fig)
+  
